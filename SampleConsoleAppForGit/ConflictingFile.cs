31dba57282adc52f02fdecb89c6b0578df1707cc
Nikki Barros <nikkib@magenic.com>
2019-01-24 15:15:37 +0800

    Add conflicting file

---
+++ SampleConsoleAppForGit/ConflictingFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleConsoleAppForGit
+{
+    class ConflictingFile
+    {
+        void ConflictingMethod()
+        {
+            Console.WriteLine("This is message 1");
+        }
+    }
+}
